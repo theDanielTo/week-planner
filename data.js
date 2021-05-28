eae49e66849693945d77746e21f622bcfc1c5944
theDanielTo <dto1989@gmail.com>
2021-05-28 16:09:06 -0700

    local storage

---
+++ data.js
@@ -0,0 +1,17 @@
+var entries = {
+  Sunday: [],
+  Monday: [],
+  Tuesday: [],
+  Wednesday: [],
+  Thursday: [],
+  Friday: [],
+  Saturday: []
+};
+
+const prevEntriesJSON = window.localStorage.getItem('schedule-local-storage');
+if (prevEntriesJSON !== null) entries = JSON.parse(prevEntriesJSON);
+
+window.addEventListener('beforeunload', function () {
+  const entriesJSON = JSON.stringify(entries);
+  window.localStorage.setItem('schedule-local-storage', entriesJSON);
+});
--- main.js
+++ main.js
@@ -2,22 +2,12 @@ var $addEntryBtn = document.querySelector('.add-entry');
 var $modalBox = document.querySelector('.modal');
 var $buttonSelect = document.querySelector('.button-select');
 var $allDays = document.querySelectorAll('.day-container');
-var $currentDay = document.querySelector("#current-day");
-var $tableData1 = document.querySelectorAll(".tableData1");
-var $tableData2 = document.querySelectorAll(".tableData2");
-var $daySelect = document.querySelector(".day-select");
-var $timeSelect = document.querySelector(".time-select");
-var $descSelect = document.querySelector(".desc-select")
-
-var entries = {
-  Sunday: [],
-  Monday: [],
-  Tuesday: [],
-  Wednesday: [],
-  Thursday: [],
-  Friday: [],
-  Saturday: []
-};
+var $currentDay = document.querySelector('#current-day');
+var $tableData1 = document.querySelectorAll('.tableData1');
+var $tableData2 = document.querySelectorAll('.tableData2');
+var $daySelect = document.querySelector('.day-select');
+var $timeSelect = document.querySelector('.time-select');
+var $descSelect = document.querySelector('.desc-select');
 
 $addEntryBtn.addEventListener('click', function (event) {
   $modalBox.classList.remove('hidden');
@@ -25,19 +15,54 @@ $addEntryBtn.addEventListener('click', function (event) {
 
 $buttonSelect.addEventListener('click', function (event) {
   $modalBox.classList.add('hidden');
-  console.log($daySelect.value);
-  console.log($timeSelect.value);
-  entries[$daySelect.value].time = $timeSelect.value;
-  entries[$daySelect.value].description = $descSelect.value;
-  console.log(entries);
+  entries[$daySelect.value].push({ time: $timeSelect.value, description: $descSelect.value });
+
+  for (const data of $tableData1) {
+    data.innerHTML = '&ThinSpace;';
+  }
+  for (const data of $tableData2) {
+    data.innerHTML = '&ThinSpace;';
+  }
+  for (let i = 0; i < entries[$daySelect.value].length; i++) {
+    $tableData1[i].innerHTML = entries[$daySelect.value][i].time;
+    $tableData2[i].innerHTML = entries[$daySelect.value][i].description;
+  }
+  $currentDay.innerHTML = $daySelect.value;
+  $daySelect.value = '';
+  $timeSelect.value = '';
+  $descSelect.value = '';
 });
 
 for (const day of $allDays) {
   day.addEventListener('click', function (event) {
-    for (let i = 0; i < entries.length; i++) {
-      $tableData1[i].innerHTML = entries[i].time;
-      $tableData2[i].innerHTML = entries[i].description;
+    for (const data of $tableData1) {
+      data.innerHTML = '&ThinSpace;';
+    }
+    for (const data of $tableData2) {
+      data.innerHTML = '&ThinSpace;';
+    }
+    for (let i = 0; i < entries[event.target.textContent].length; i++) {
+      $tableData1[i].innerHTML = entries[event.target.textContent][i].time;
+      $tableData2[i].innerHTML = entries[event.target.textContent][i].description;
     }
     $currentDay.innerHTML = event.target.textContent;
   });
 }
+
+window.addEventListener('DOMContentLoaded', function (event) {
+  for (const day in entries) {
+    for (const data of $tableData1) {
+      data.innerHTML = '&ThinSpace;';
+    }
+    for (const data of $tableData2) {
+      data.innerHTML = '&ThinSpace;';
+    }
+    for (let i = 0; i < entries[day].length; i++) {
+      console.log(entries[day][i]);
+      $tableData1[i].innerHTML = entries[day][i].time;
+      $tableData2[i].innerHTML = entries[day][i].description;
+    }
+    $currentDay.innerHTML = 'Monday';
+  }
+
+});
