aa72381ee669a53ea56eccea41e3e0892b59515a
John Hwang <johnhchwang@gmail.com>
2021-05-28 12:44:37 -0700

    current work

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 var $addEntryBtn = document.querySelector('.add-entry');
 var $modalBox = document.querySelector('.modal');
+var $buttonSelect = document.querySelector('.button-select');
 
 $addEntryBtn.addEventListener('click', function (event) {
   $modalBox.classList.remove('hidden');
+});
+
+$buttonSelect.addEventListener('click', function (event) {
+  $modalBox.classList.add('hidden');
+
 });
